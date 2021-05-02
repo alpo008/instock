623b784af88d5163f572329885deaaba3659d2d0
Alexey POZHIDAEV <pozhydaev_alexey@mail.ru>
2021-05-02 23:32:18 +0300

    Material groups (machines) form

--- modules/admin/controllers/SettingsController.php
+++ modules/admin/controllers/SettingsController.php
@@ -36,9 +36,30 @@ class SettingsController extends Controller
         return $this->render('material-export-format-form', compact('model', 'columns'));
     }
 
-    public function actionMaterialGroups()
+    /**
+     * @return string
+     */
+    public function actionMaterialGroups(): string
     {
         $fileStorage = new FileStorage();
+        $materialGroups = $fileStorage->getContent(Material::GROUPS_LIST_STORAGE);
+        if ($post = \Yii::$app->request->post()) {
+            if (!empty($post['action']) && !empty($post['group']) && $post['action'] === 'delete') {
+                if (array_key_exists($post['group'], $materialGroups)) {
+                    unset($materialGroups[$post['group']]);
+                    $fileStorage->setContent(Material::GROUPS_LIST_STORAGE, $materialGroups);
+                }
+            } elseif (!empty($post['materialGroups']) && is_array($post['materialGroups'])) {
+                $updatedMaterialGroups = [];
+                foreach ($post['materialGroups'] as $materialGroup) {
+                    if (!array_key_exists($materialGroup, $updatedMaterialGroups) && !empty($materialGroup)) {
+                        $updatedMaterialGroups[$materialGroup] = $materialGroup;
+                    }
+                }
+                ksort($updatedMaterialGroups);
+                $fileStorage->setContent(Material::GROUPS_LIST_STORAGE, $updatedMaterialGroups);
+            }
+        }
         $materialGroups = $fileStorage->getContent(Material::GROUPS_LIST_STORAGE);
 
         return $this->render('material-groups-form', compact('materialGroups'));
--- modules/admin/views/settings/material-groups-form.php
+++ modules/admin/views/settings/material-groups-form.php
@@ -1,7 +1,79 @@
 <?php
 
 /* @var $this yii\web\View */
-/* @var $model \app\modules\admin\models\MaterialExport */
-/* @var $columns array */
+/* @var $materialGroups array */
 
+use yii\bootstrap4\Html;
+use rmrevin\yii\fontawesome\FAS;
+use yii\widgets\Pjax;
+
+$this->title = Yii::t('app', 'Groups');
+$this->params['breadcrumbs'][] = Yii::t('app', 'Settings') . ' > ' . $this->title;
+
+Pjax::begin(['id' => 'settings-groups-pjax-container'])
 ?>
+<?= Html::beginForm(['settings/material-groups'], 'post', [
+    'id' => 'settings-groups-form',
+    'class' => 'settings-form',
+    'data-pjax' => '1'
+]) ?>
+    <table class="table table-borderless">
+        <?php if (!empty($materialGroups) && is_array($materialGroups)) : ?>
+            <?php foreach ($materialGroups as $materialGroup) : ?>
+                <tr>
+                    <td>
+                        <div class="form-group">
+                            <?= Html::textInput("materialGroups[$materialGroup]", $materialGroup, [
+                                'class' => 'form-control',
+                                'maxlength' => 16
+                            ]) ?>
+                        </div>
+                    </td>
+                    <td>
+                        <?= Html::a(
+                                FAS::icon('trash-alt'),
+                                ['settings/material-groups'],
+                                [
+                                    'data' => [
+                                        'pjax' => '1',
+                                        'method' => 'post',
+                                        'params' => [
+                                            'action' => 'delete',
+                                            'group' => $materialGroup
+                                        ]
+                                    ],
+                                    'title' => Yii::t('app', 'Delete group')
+                                ]
+                            );
+                        ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <tr>
+            <td>
+                <div class="form-group">
+                    <?= Html::textInput("materialGroups[new]", '', [
+                        'class' => 'form-control',
+                        'placeholder' => Yii::t('app', 'Enter new machine name'),
+                        'maxlength' => 16
+                    ]) ?>
+                </div>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <div class="form-group action-buttons">
+                    <?= Html::submitButton(FAS::icon('save'), [
+                        'class' => 'btn btn-success',
+                        'style' => 'margin-right:-40px',
+                        'title' => Yii::t('app', 'Save')
+                    ]) ?>
+                </div>
+            </td>
+            <td></td>
+        </tr>
+    </table>
+<?php echo Html::endForm();
+Pjax::end();
